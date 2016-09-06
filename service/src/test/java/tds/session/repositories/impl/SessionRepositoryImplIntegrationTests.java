a8402a1209b6101986aa95313aed5276e5902b49
Todd Miller <tmiller@fairwaytech.com>
2016-09-06 11:22:00 -0700

    Added flyway migration and integration test for the repository

---
+++ service/src/test/java/tds/session/repositories/impl/SessionRepositoryImplIntegrationTests.java
@@ -0,0 +1,33 @@
+package tds.session.repositories.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import tds.session.Session;
+import tds.session.SessionServiceApplication;
+import tds.session.repositories.SessionRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SessionServiceApplication.class)
+public class SessionRepositoryImplIntegrationTests {
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @Test
+    public void shouldRetrieveSessionForId() {
+        UUID sessionId = UUID.fromString("06485031-B2B6-4CED-A0C1-B294EDA54DB2");
+        Optional<Session> sessionOptional = sessionRepository.getSessionById(sessionId);
+        assertThat(sessionOptional.isPresent()).isTrue();
+        assertThat(sessionOptional.get().getId()).isEqualTo(sessionId);
+        assertThat(sessionOptional.get().getStatus()).isEqualTo("closed");
+        assertThat(sessionOptional.get().getType()).isEqualTo(0);
+    }
+
+}
