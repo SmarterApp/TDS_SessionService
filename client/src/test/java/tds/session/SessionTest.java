6709d7574c66a4cad404f3943d04ca6718f26482
Todd Miller <tmiller@fairwaytech.com>
2016-09-06 17:04:54 -0700

    Added unit testing to the client jar

---
+++ client/src/test/java/tds/session/SessionTest.java
@@ -0,0 +1,21 @@
+package tds.session;
+
+import org.junit.Test;
+
+import java.util.UUID;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SessionTest {
+    @Test
+    public void shouldBeInstantiated() {
+        Session session = new Session();
+        UUID sessionId = UUID.randomUUID();
+        session.setId(sessionId);
+        session.setType(0);
+        session.setStatus("closed");
+
+        assertThat(session.getId()).isEqualTo(sessionId);
+        assertThat(session.getType()).isEqualTo(0);
+        assertThat(session.getStatus()).isEqualTo("closed");
+    }
+}
