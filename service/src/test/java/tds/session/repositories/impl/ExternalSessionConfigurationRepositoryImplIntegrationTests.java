da93bc6226f10abb9d5413004356e6a8a52236c5
Todd Miller <tmiller@fairwaytech.com>
2016-10-19 15:12:26 -0700

    More migration cleanup

--- service/src/test/java/tds/session/repositories/impl/ExternalSessionConfigurationRepositoryImplIntegrationTests.java
+++ service/src/test/java/tds/session/repositories/impl/ExternalSessionConfigurationRepositoryImplIntegrationTests.java
@@ -1,13 +1,20 @@
 package tds.session.repositories.impl;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.sql.DataSource;
 import java.util.Optional;
+import java.util.UUID;
 
+import tds.common.data.mysql.UuidAdapter;
 import tds.session.ExternalSessionConfiguration;
 import tds.session.repositories.ExternalSessionConfigurationRepository;
 
@@ -15,15 +22,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@Transactional
 public class ExternalSessionConfigurationRepositoryImplIntegrationTests {
     @Autowired
     private ExternalSessionConfigurationRepository externalSessionConfigurationRepository;
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Before
+    public void setUp() {
+        String externsInsertSQL = "INSERT INTO session._externs (clientname, environment) VALUES ('SESSION-SBAC', 'Development');";
+        String clientExternsInsertSQL = "INSERT INTO configs.client_externs VALUES (:uuid,'MultiClient_RTS_2013','MultiClient_RTS_2013','itembank',1,1,'RTS','RTS',1,'session',1,1,'SESSION-SBAC',1,'SESSION-SBAC',NULL,'Development',0,0,NULL,100000,1,NULL,NULL);";
+
+        NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        jdbcTemplate.update(externsInsertSQL, new MapSqlParameterSource());
+        jdbcTemplate.update(clientExternsInsertSQL, new MapSqlParameterSource("uuid", UuidAdapter.getBytesFromUUID(UUID.randomUUID())));
+    }
+
     @Test
     public void shouldFindExternalSessionConfigurationByClientName() {
-        Optional<ExternalSessionConfiguration> maybeExternalSessionConfiguration = externalSessionConfigurationRepository.findExternalSessionConfigurationByClientName("SBAC");
+        Optional<ExternalSessionConfiguration> maybeExternalSessionConfiguration = externalSessionConfigurationRepository.findExternalSessionConfigurationByClientName("SESSION-SBAC");
         assertThat(maybeExternalSessionConfiguration).isPresent();
-        assertThat(maybeExternalSessionConfiguration.get().getClientName()).isEqualTo("SBAC");
+        assertThat(maybeExternalSessionConfiguration.get().getClientName()).isEqualTo("SESSION-SBAC");
         assertThat(maybeExternalSessionConfiguration.get().getEnvironment()).isEqualTo("Development");
     }
 
