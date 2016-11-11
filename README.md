1c9d6dd4304dfff3743eca880def455112a337bd
Todd Miller <tmiller@fairwaytech.com>
2016-11-11 09:26:01 -0800

    Build the client jar with Java 1.7

--- README.md
+++ README.md
@@ -5,6 +5,40 @@ The `TDS_Session` (aka Session Support Service) consists of two modules:
 * **client:** Contains the POJOs/classes needed for a consumer to interact with the Session Support Service
 * **service:** REST endpoints that provide TDS session data
 
+
+## Setup
+There are a few things you will need to do to build the project.  The following tools will be needed to build the project.
+
+1. Maven
+2. Java 7 - for the client project
+3. Java 8 - for the server project
+
+In addition to the tools the project also uses [Maven toolchains](https://maven.apache.org/guides/mini/guide-using-toolchains.html) since the client needs to be built at Java 1.7 to support legacy applications.  You will need to either append to your current toolchain file or create a new one in your local .m2 directory.
+
+```<?xml version="1.0" encoding="UTF8"?>
+<toolchains>
+  <!-- JDK toolchains -->
+  <toolchain>
+    <type>jdk</type>
+    <provides>
+      <version>1.7</version>
+    </provides>
+    <configuration>
+      <jdkHome>/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home</jdkHome>
+    </configuration>
+  </toolchain>
+  <toolchain>
+    <type>jdk</type>
+    <provides>
+      <version>1.8</version>
+    </provides>
+    <configuration>
+      <jdkHome>/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home</jdkHome>
+    </configuration>
+  </toolchain>
+</toolchains>
+```
+
 ## Build
 To build the **client** and **service**, use the "parent" `pom.xml` that is contained in the `TDS_SessionService` directory:
 
--- service/src/test/java/tds/session/repositories/impl/SessionRepositoryImplIntegrationTests.java
+++ service/src/test/java/tds/session/repositories/impl/SessionRepositoryImplIntegrationTests.java
@@ -1,14 +1,20 @@
 package tds.session.repositories.impl;
 
+import org.joda.time.Instant;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.sql.DataSource;
 import java.text.ParseException;
-import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,36 +22,108 @@ import tds.session.Session;
 import tds.session.repositories.SessionRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static tds.common.data.mapping.ResultSetMapperUtility.mapInstantToTimestamp;
+import static tds.common.data.mysql.UuidAdapter.getBytesFromUUID;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @Transactional
 public class SessionRepositoryImplIntegrationTests {
+    private final static String sessionInsertSQL = "insert into session (\n" +
+        "  _key,\n" +
+        "  datecreated,\n" +
+        "  clientname,\n" +
+        "  _fk_browser,\n" +
+        "  environment,\n" +
+        "  status,\n" +
+        "  _efk_proctor,\n" +
+        "  datechanged,\n" +
+        "  dateend,\n" +
+        "  datebegin, \n" +
+        "  datevisited \n" +
+        ") values (\n" +
+        "  :key,\n" +
+        "  :dateCreated,\n" +
+        "  :clientName,\n" +
+        "  :browserId,\n" +
+        "  :environment,\n" +
+        "  :status,\n" +
+        "  :proctorId,\n" +
+        "  :dateChanged,\n" +
+        "  :dateEnd,\n" +
+        "  :dateBegin,\n" +
+        "  :dateVisited\n" +
+        ");";
 
     @Autowired
     private SessionRepository sessionRepository;
 
+    @Autowired
+    private DataSource dataSource;
+
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Before
+    public void setUp() {
+        jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
     @Test
     public void shouldRetrieveSessionForId() throws ParseException {
-        UUID sessionId = UUID.fromString("06485031-B2B6-4CED-A0C1-B294EDA54DB2");
+        UUID sessionId = UUID.randomUUID();
+        UUID browserKey = UUID.randomUUID();
+
+        Instant dateBegin = Instant.now().minus(10000);
+        Instant dateVisited = Instant.now().minus(8000);
+        Instant dateChanged = Instant.now().minus(5000);
+        Instant dateEnded = Instant.now();
+
+        Session session = new Session.Builder()
+            .withId(sessionId)
+            .withClientName("SBAC_PT")
+            .withBrowserKey(browserKey)
+            .withStatus("closed")
+            .withProctorId(23L)
+            .withDateBegin(dateBegin)
+            .withDateVisited(dateVisited)
+            .withDateChanged(dateChanged)
+            .withDateEnd(dateEnded)
+            .build();
+
+        insertSession(session);
 
         Optional<Session> sessionOptional = sessionRepository.findSessionById(sessionId);
         assertThat(sessionOptional).isPresent();
         assertThat(sessionOptional.get().getId()).isEqualTo(sessionId);
         assertThat(sessionOptional.get().getType()).isEqualTo(0);
         assertThat(sessionOptional.get().getStatus()).isEqualTo("closed");
-        assertThat(sessionOptional.get().getDateBegin()).isEqualTo(Instant.parse("2016-08-18T18:25:07.115Z"));
-        assertThat(sessionOptional.get().getDateEnd()).isEqualTo(Instant.parse("2016-08-18T18:26:31.669Z"));
-        assertThat(sessionOptional.get().getDateChanged()).isEqualTo(Instant.parse("2016-08-18T18:26:31.669Z"));
-        assertThat(sessionOptional.get().getDateVisited()).isEqualTo(Instant.parse("2016-08-18T18:25:07.115Z"));
+        assertThat(sessionOptional.get().getDateBegin()).isEqualTo(dateBegin);
+        assertThat(sessionOptional.get().getDateChanged()).isEqualTo(dateChanged);
+        assertThat(sessionOptional.get().getDateEnd()).isEqualTo(dateEnded);
+        assertThat(sessionOptional.get().getDateVisited()).isEqualTo(dateVisited);
         assertThat(sessionOptional.get().getClientName()).isEqualTo("SBAC_PT");
-        assertThat(sessionOptional.get().getProctorId()).isEqualTo(5);
-        assertThat(sessionOptional.get().getBrowserKey()).isEqualTo(UUID.fromString("CB5C658D-4B32-463D-9DFA-119052E27474"));
+        assertThat(sessionOptional.get().getProctorId()).isEqualTo(23);
+        assertThat(sessionOptional.get().getBrowserKey()).isEqualTo(browserKey);
     }
 
     @Test
     public void shouldReturnASessionForASessionIdThatHasNullDates() {
-        UUID sessionId = UUID.fromString("A976E970-F80C-4107-830E-B1020053DE96");
+        UUID sessionId = UUID.randomUUID();
+        UUID browserKey = UUID.randomUUID();
+
+        Session session = new Session.Builder()
+            .withId(sessionId)
+            .withClientName("SBAC_PT")
+            .withBrowserKey(browserKey)
+            .withStatus("open")
+            .withProctorId(99L)
+            .build();
+
+        insertSession(session);
 
         Optional<Session> sessionOptional = sessionRepository.findSessionById(sessionId);
         assertThat(sessionOptional).isPresent();
@@ -56,8 +134,8 @@ public class SessionRepositoryImplIntegrationTests {
         assertThat(sessionOptional.get().getDateEnd()).isNull();
         assertThat(sessionOptional.get().getDateChanged()).isNull();
         assertThat(sessionOptional.get().getDateVisited()).isNull();
-        assertThat(sessionOptional.get().getProctorId()).isEqualTo(6);
-        assertThat(sessionOptional.get().getBrowserKey()).isEqualTo(UUID.fromString("F7A0375C-C63A-4164-976E-E883C2D13F62"));
+        assertThat(sessionOptional.get().getProctorId()).isEqualTo(99L);
+        assertThat(sessionOptional.get().getBrowserKey()).isEqualTo(browserKey);
     }
 
     @Test
@@ -68,7 +146,22 @@ public class SessionRepositoryImplIntegrationTests {
 
     @Test
     public void shouldPauseASession() {
-        UUID sessionId = UUID.fromString("08A57E3F-3A87-44C5-82A6-5B473E60785E");
+        UUID sessionId = UUID.randomUUID();
+        UUID browserKey = UUID.randomUUID();
+
+        Instant dateBegin = Instant.now().minus(10000);
+
+        Session session = new Session.Builder()
+            .withId(sessionId)
+            .withClientName("SBAC_PT")
+            .withBrowserKey(browserKey)
+            .withStatus("open")
+            .withProctorId(99L)
+            .withDateBegin(dateBegin)
+            .build();
+
+        insertSession(session);
+
         final String status = "unit test";  // represents the status change sent in from the caller
 
         sessionRepository.pause(sessionId, status);
@@ -81,5 +174,21 @@ public class SessionRepositoryImplIntegrationTests {
         assertThat(result.get().getDateChanged()).isGreaterThan(result.get().getDateBegin());
         assertThat(result.get().getDateEnd()).isNotNull();
         assertThat(result.get().getDateEnd()).isGreaterThan(result.get().getDateBegin());
+    }
+
+    private void insertSession(Session session) {
+        SqlParameterSource parameters = new MapSqlParameterSource("key", getBytesFromUUID(session.getId()))
+            .addValue("clientName", session.getClientName())
+            .addValue("browserId", getBytesFromUUID(session.getBrowserKey()))
+            .addValue("environment", "unitTest")
+            .addValue("status", session.getStatus())
+            .addValue("proctorId", session.getProctorId())
+            .addValue("dateChanged", mapInstantToTimestamp(session.getDateChanged()))
+            .addValue("dateVisited", mapInstantToTimestamp(session.getDateVisited()))
+            .addValue("dateBegin", mapInstantToTimestamp(session.getDateBegin()))
+            .addValue("dateCreated", mapInstantToTimestamp(Instant.now()))
+            .addValue("dateEnd", mapInstantToTimestamp(session.getDateEnd()));
+
+        jdbcTemplate.update(sessionInsertSQL, parameters);
     }
 }
