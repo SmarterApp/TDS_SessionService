c1951b37595f53988b95817788775bba83eb5af8
Todd Miller <tmiller@fairwaytech.com>
2016-12-27 20:00:35 -0800

    Add Jackson Object Mapper Configuration

--- service/src/main/java/tds/session/configuration/SessionApplicationConfiguration.java
+++ service/src/main/java/tds/session/configuration/SessionApplicationConfiguration.java
@@ -1,10 +1,17 @@
 package tds.session.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import tds.common.configuration.DataSourceConfiguration;
+import tds.common.configuration.JacksonObjectMapperConfiguration;
 import tds.common.web.advice.ExceptionAdvice;
 
-@Import({ExceptionAdvice.class, DataSourceConfiguration.class})
+@Configuration
+@Import({
+    ExceptionAdvice.class,
+    DataSourceConfiguration.class,
+    JacksonObjectMapperConfiguration.class
+})
 public class SessionApplicationConfiguration {
 }
