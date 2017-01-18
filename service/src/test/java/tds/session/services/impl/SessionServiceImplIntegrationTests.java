9d97d476257146f9329d688d930e1d86b80ac970
burzum619 <kingernest@gmail.com>
2017-01-17 16:28:34 -0800

    SessionService caching and integration test (#22)

---
+++ service/src/test/java/tds/session/services/impl/SessionServiceImplIntegrationTests.java
@@ -0,0 +1,46 @@
+package tds.session.services.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import tds.session.Session;
+import tds.session.repositories.SessionRepository;
+import tds.session.services.SessionService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class SessionServiceImplIntegrationTests {
+    @MockBean
+    private SessionRepository mockSessionRepository;
+
+    @Autowired
+    private SessionService sessionService;
+
+    @Test
+    public void shouldReturnCachedSession() {
+        UUID id = UUID.randomUUID();
+        Session session = new Session.Builder()
+            .withId(id)
+            .build();
+        when(mockSessionRepository.findSessionById(id)).thenReturn(Optional.of(session));
+
+        Optional<Session> sessionOptional = sessionService.findSessionById(id);
+
+        assertThat(sessionOptional).isPresent();
+        assertThat(sessionOptional.get().getId()).isEqualTo(id);
+
+        verify(mockSessionRepository, times(1)).findSessionById(id);
+    }
+}
